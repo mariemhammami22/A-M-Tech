722c6128ca72637fbf1b0d81747b7cc87d5de175
aya ahmed <eya.ahmed19@gmail.com>
2018-11-08 15:56:57 +0100

    commit_msg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
+import { SliderItemComponent } from './slider-item/slider-item.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesFormComponent } from './messages-form/messages-form.component';
+import { ContactHeaderComponent } from './contact-header/contact-header.component';
+import { ProductsComponent } from './products/products.component';
+
+const appRoutes: Routes = [
+  { path: 'contact-page', component: ContactPageComponent},
+  { path: 'home', component: HomeComponent},
+
+];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    FooterComponent,
+    SliderComponent,
+    SliderItemComponent,
+    ContactPageComponent,
+    HomeComponent,
+    MessagesFormComponent,
+    ContactHeaderComponent,
+    ProductsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
