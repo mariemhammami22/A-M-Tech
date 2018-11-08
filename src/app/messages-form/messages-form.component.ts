46294c911e40866d210315460240d1f5d538e844
aya ahmed <eya.ahmed19@gmail.com>
2018-11-08 15:57:52 +0100

    commit_msg

---
+++ src/app/messages-form/messages-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messages-form',
+  templateUrl: './messages-form.component.html',
+  styleUrls: ['./messages-form.component.css']
+})
+export class MessagesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/slider-item/slider-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-item',
+  templateUrl: './slider-item.component.html',
+  styleUrls: ['./slider-item.component.css']
+})
+export class SliderItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
